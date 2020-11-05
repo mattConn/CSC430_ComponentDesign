d743b0271cc77f9bf7aeb096c9f1061e17e9a5de
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-11-05 00:52:44 -0500

    moved menu class into own project and added tester project to solution

---
+++ MenuTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ComponentDesign;
+
+
+namespace MenuTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu m = new Menu();
+            Console.WriteLine(m);
+        }
+    }
+}
