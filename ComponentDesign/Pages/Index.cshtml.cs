68b9556afa600d2ee324c2977537b6c02f88c1e4
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-11-06 21:27:01 -0500

    added back in cshtml, displaying menu length in index.cshtml

---
+++ ComponentDesign/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ComponentDesign.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public static string test = "HELLO WORLD";
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ ComponentDesign/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ComponentDesign
+{
+    public class Program
+    {
+
+        public static Menu menu = new Menu("C:/Users/Matthew/source/repos/ComponentDesign2/menu_items.json");
+
+        public static void Main(string[] args)
+        {
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
